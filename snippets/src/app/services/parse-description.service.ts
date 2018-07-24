21f3a39488447ad2e2a1bd5d31f1fd602eb172df
Matt Cheah <matt.cheah@gmail.com>
2018-07-24 12:01:13 -0700

    set up quicksort for sorting keywords array by most jobKeywords. needs to be tested

--- snippets/src/app/services/parse-description.service.ts
+++ snippets/src/app/services/parse-description.service.ts
@@ -18,12 +18,14 @@ export class ParseDescriptionService {
     for(let i = 0; i < descriptionWords.length; i++) {
       let word = descriptionWords[i].toLowerCase();
       if(word in this.databaseService.categories) {
-        console.log("Description word: "+word +" is in categories");
         this.databaseService.categories[word].jobKeywords++;
       }
     }
     console.log("databaseService categories list is now:");
     console.log(this.databaseService.categories);
+
+    this.createOrderedArray();
+
   }
 
   resetJobKeywords():void {
@@ -32,4 +34,51 @@ export class ParseDescriptionService {
       this.databaseService.categories[keywords[i]].jobKeywords = 0;
     }
   }
+
+  createOrderedArray():Array<{}> {
+    let keywords = Object.keys(this.databaseService.categories);
+    let keywordsArray = [];
+    for (let i = 0; i < keywords.length; i++) {
+      this.databaseService.categories[keywords[i]].keyword = keywords[i];
+      keywordsArray.push(this.databaseService.categories[keywords[i]]);
+    }
+    console.log("KeywordsArray before:");
+    console.log(keywordsArray);
+
+
+    function quicksortByJobKeywords(arr, left, right) {
+      if (left < right) {
+        let divider = divide(arr, left, right);
+        quicksortByJobKeywords(arr, left, divider-1);
+        quicksortByJobKeywords(arr, divider+1, right);
+      }
+      return arr;
+    }
+
+    function divide(arr,left,right) {
+      let dividerIndex = left;
+      let pivot = right;
+
+      for(let i = left; i < right; i++) {
+        if (arr[i].jobKeywords > arr[pivot].jobKeywords) {
+          swap(arr, i, dividerIndex);
+          dividerIndex++;
+        }
+      }
+      swap(arr, pivot, dividerIndex);
+      return dividerIndex;
+    }
+
+    function swap(arr, a, b) {
+      let temp = arr[a];
+      arr[a] = arr[b];
+      arr[b] = temp;
+    }
+
+    quicksortByJobKeywords(keywordsArray, 0, keywordsArray.length-1);
+    console.log("KeywordsArray after:");
+    console.log(keywordsArray);
+    return keywordsArray;
+  }
+
 }
