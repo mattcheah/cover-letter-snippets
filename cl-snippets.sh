6ac059e269e0703ae9038da9f641d3808dca3989
Matt Cheah <matt.cheah@gmail.com>
2018-07-27 11:45:24 -0700

    attempt to get rid of .sh file and just run index.js as executable

--- cl-snippets.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Find directory of bin link that calls cl-snippets.sh, cd in and run snippets.
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-cd "${DIR}" || exit
-
-node index.js
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 
 const express = require('express');
 const opn = require('opn');
