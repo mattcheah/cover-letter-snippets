ed98f0b00ee3668bdb6302559133f15698969ef8
Matt Cheah <matt.cheah@gmail.com>
2018-07-27 11:26:36 -0700

    fix snippet start command sh script

--- cl-snippets.sh
+++ cl-snippets.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
+# Find directory of bin link that calls cl-snippets.sh, cd in and run snippets.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd "${DIR}" || exit
+
 node index.js
