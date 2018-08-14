58dd0c7564d15c0e3ff8caf57e22caf2a74ed8ec
Matt Cheah <mcheah@bisnarchase.com>
2018-08-14 09:34:26 -0700

    wrote cover letter service tests

--- snippets/src/app/app.component.spec.ts
+++ snippets/src/app/app.component.spec.ts
@@ -27,10 +27,10 @@ describe('AppComponent', () => {
   //   const app = fixture.debugElement.componentInstance;
   //   expect(app.title).toEqual('app');
   // }));
-  // it('should render title in a h1 tag', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  // }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Cover Letter Snippets!');
+  }));
 });
--- snippets/src/app/services/parse-description.service.ts
+++ snippets/src/app/services/parse-description.service.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-import { DatabaseService } from "./database.service";
+import { DatabaseService } from './database.service';
 import { StatusMessageService} from './status-message.service';
 
 @Injectable()
 export class ParseDescriptionService {
 
-  jobDescription:string = "";
-  showParsingResults:boolean = false;
-  keywordArray:Array<{}>;
+  jobDescription = '';
+  showParsingResults = false;
+  keywordArray: Array<{}>;
 
-  constructor(private databaseService:DatabaseService, private statusMessageService:StatusMessageService) { }
+  constructor(private databaseService: DatabaseService, private statusMessageService: StatusMessageService) { }
 
-  parseDescription(description):any {
+  parseDescription(description): any {
     this.resetJobKeywords();
     this.jobDescription = description;
 
-    let descriptionWords = description.split(/\W/);
+    const descriptionWords = description.split(/\W/);
 
-    for(let i = 0; i < descriptionWords.length; i++) {
-      let word = descriptionWords[i].toLowerCase();
-      if(word in this.databaseService.categories) {
+    for (let i = 0; i < descriptionWords.length; i++) {
+      const word = descriptionWords[i].toLowerCase();
+      if (word in this.databaseService.categories) {
         this.databaseService.categories[word].jobKeywords++;
       }
     }
@@ -30,16 +30,16 @@ export class ParseDescriptionService {
     this.showParsingResults = true;
   }
 
-  resetJobKeywords():void {
-    let keywords = Object.keys(this.databaseService.categories);
-    for(let i = 0; i< keywords.length; i++) {
+  resetJobKeywords(): void {
+    const keywords = Object.keys(this.databaseService.categories);
+    for (let i = 0; i < keywords.length; i++) {
       this.databaseService.categories[keywords[i]].jobKeywords = 0;
     }
   }
 
-  createOrderedArray():Array<{}> {
-    let keywords = Object.keys(this.databaseService.categories);
-    let keywordsArray = [];
+  createOrderedArray(): Array<{}> {
+    const keywords = Object.keys(this.databaseService.categories);
+    const keywordsArray = [];
     for (let i = 0; i < keywords.length; i++) {
       this.databaseService.categories[keywords[i]].keyword = keywords[i];
       keywordsArray.push(this.databaseService.categories[keywords[i]]);
@@ -47,18 +47,18 @@ export class ParseDescriptionService {
 
     function quicksortByJobKeywords(arr, left, right) {
       if (left < right) {
-        let divider = divide(arr, left, right);
-        quicksortByJobKeywords(arr, left, divider-1);
-        quicksortByJobKeywords(arr, divider+1, right);
+        const divider = divide(arr, left, right);
+        quicksortByJobKeywords(arr, left, divider - 1);
+        quicksortByJobKeywords(arr, divider + 1, right);
       }
       return arr;
     }
 
-    function divide(arr,left,right) {
+    function divide(arr, left, right) {
       let dividerIndex = left;
-      let pivot = right;
+      const pivot = right;
 
-      for(let i = left; i < right; i++) {
+      for (let i = left; i < right; i++) {
         if (arr[i].jobKeywords > arr[pivot].jobKeywords) {
           swap(arr, i, dividerIndex);
           dividerIndex++;
@@ -69,14 +69,14 @@ export class ParseDescriptionService {
     }
 
     function swap(arr, a, b) {
-      let temp = arr[a];
+      const temp = arr[a];
       arr[a] = arr[b];
       arr[b] = temp;
     }
 
-    quicksortByJobKeywords(keywordsArray, 0, keywordsArray.length-1);
-    let statusMessage = "Finished parsing the description. The category with the most keywords in this job description is: "+keywordsArray[0].keyword + " with "+keywordsArray[0].jobKeywords + " keywords.";
-    this.statusMessageService.newStatusMessage(statusMessage, "success");
+    quicksortByJobKeywords(keywordsArray, 0, keywordsArray.length - 1);
+    const statusMessage = 'Finished parsing the description. The category with the most keywords in this job description is: ' + keywordsArray[0].keyword + ' with ' + keywordsArray[0].jobKeywords + ' keywords.';
+    this.statusMessageService.newStatusMessage(statusMessage, 'success');
     return keywordsArray;
   }
 
